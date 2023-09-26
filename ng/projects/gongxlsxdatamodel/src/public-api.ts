470490646b7d465736b232f265d24e9bc8f7d5eb
peugeot <thomaspeugeot@yahoo.fr>
2023-09-27 00:40:42 +0200

    cleanup

--- ng/projects/gongxlsxdatamodel/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of gongxlsxdatamodel
- */
-
-export * from './lib/gongxlsxdatamodel.service';
-export * from './lib/gongxlsxdatamodel.component';
-export * from './lib/gongxlsxdatamodel.module';
